64f69f085a3bd0897edef01127c7b7d00618b57c
code-review-interview <udokiminage+code-review-interview@gmail.com>
2023-07-29 23:07:48 -0400

    Interview changes

---
+++ src/main/java/interview/code/review/Group.java
@@ -0,0 +1,16 @@
+package interview.code.review;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Group {
+
+    String name;
+    String schoolName;
+}
